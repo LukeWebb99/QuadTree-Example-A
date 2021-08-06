110f5b838d9c570485163cc5ecc7f302c6feaec0
LukeWebb99 <49219052+LukeWebb99@users.noreply.github.com>
2021-08-06 22:14:58 +0100

    minor update

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,16 +8,18 @@ int main()
 
     srand(time(0));
 
+    const int windowWidth = 800;
+    const int windowHegiht = 800;
   
-    Rectangle* boundary = new Rectangle(400, 400, 400, 400);
+    Rectangle* boundary = new Rectangle(windowWidth, windowHegiht, windowWidth, windowHegiht);
     QuadTree* qt = new QuadTree(boundary, 4);
 
     for (size_t i = 0; i < 500; i++) {
-        Point* p = new Point(rand() % 400, rand() % 400);
+        Point* p = new Point(rand() % windowWidth, rand() % windowHegiht);
         qt->Insert(p);
     }
 
-    sf::RenderWindow window(sf::VideoMode(400, 400), "");
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHegiht), "Quadtree", sf::Style::Close);
     while (window.isOpen())
     {
         sf::Event event;
